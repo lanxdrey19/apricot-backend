75808ee60561198eeec7624b097952c4d7f408d1
Lance Delos Reyes <lan_drey19@hotmail.com>
2022-02-07 00:55:54 +1300

    :card_file_box: establish connection to database

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
+require("dotenv/config");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use("/", () => {
-  console.log("hello");
+  console.log("middleware called");
+});
+
+mongoose.connect(process.env.DB_CONNECTION, () => {
+  console.log("Connection to db established");
 });
 
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send("/ is called");
 });
