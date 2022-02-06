974e04d06ffbf021cb43a2f5f54e4cf7bfe0c6a1
Lance Delos Reyes <lan_drey19@hotmail.com>
2022-02-07 12:50:52 +1300

    :sparkles: add creation and serial increment endpoint

---
+++ entities/Template.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const TemplateSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  group: {
+    type: String,
+    required: true,
+  },
+  era: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  serial: {
+    type: Number,
+    default: 0,
+  },
+});
+
+module.exports = mongoose.model("Templates", TemplateSchema);
