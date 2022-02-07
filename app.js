ba93392a00a95a31d91839bf2a676412d7346d7f
Lance Delos Reyes <lan_drey19@hotmail.com>
2022-02-07 16:13:56 +1300

    :sparkles: create endpoint specific to user information manipulation

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ require("dotenv/config");
 
 const serversRoute = require("./routes/servers");
 const templatesRoute = require("./routes/templates");
+const usersRoute = require("./routes/users");
 
 app.use(bodyParser.json());
 app.use("/servers", serversRoute);
 app.use("/templates", templatesRoute);
+app.use("/users", usersRoute);
 
 mongoose.connect(process.env.DB_CONNECTION, () => {
   console.log("Connection to db established");
---
+++ entities/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  cards: {
+    type: [[String]], // [reference to template db id, recorded serial, stars, tag name ("" if no tag) ]
+    required: true,
+  },
+  tags: {
+    type: [{ String: String }],
+    required: true,
+  },
+  nextClaimTime: {
+    type: Date,
+    default: null,
+  },
+  nextDropTime: {
+    type: Date,
+    default: null,
+  },
+  tokens: {
+    type: Number,
+    default: 0,
+  },
+});
+
+module.exports = mongoose.model("Users", UserSchema);
