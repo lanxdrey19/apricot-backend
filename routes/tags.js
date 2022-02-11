2691edf9d9e485ac768579c675f359dfb0089a1e
Lance Delos Reyes <lan_drey19@hotmail.com>
2022-02-12 01:54:34 +1300

    :recycle: refactor endpoints for tags

--- routes/tags.js
+++ routes/tags.js
@@ -4,31 +4,39 @@ const Server = require("../entities/Server");
 const User = require("../entities/User");
 const Template = require("../entities/Template");
 
+router.get("/:userId", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      userId: req.params.userId,
+    });
+
+    res.status(200).json(user.tags);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
 router.patch("/add", async (req, res) => {
   try {
     const user = await User.findOne({
       userId: req.body.userId,
     });
     let isValid = true;
-
     user.tags.forEach((tagInfo) => {
-      if (tagInfo[0].toLowerCase() === req.body.tagName.toLowerCase()) {
+      if (tagInfo.tagName.toLowerCase() === req.body.tagName.toLowerCase()) {
         isValid = false;
       }
     });
     if (isValid) {
-      await User.updateOne(
+      let tag = {
+        tagName: req.body.tagName.toString().toLowerCase(),
+        tagEmote: req.body.tagEmote.toString().toLowerCase(),
+      };
+      const updatedUser = await User.updateOne(
         { userId: req.body.userId },
-        {
-          $push: {
-            tags: [
-              req.body.tagName.toString().toLowerCase(),
-              req.body.tagEmote.toString().toLowerCase(),
-            ],
-          },
-        }
+        { $push: { tags: tag } }
       );
-      res.status(204).json({ message: "success" });
+      res.json(updatedUser);
     } else {
       res.status(400).json({ message: "error" });
     }
@@ -47,22 +55,22 @@ router.patch("/delete", async (req, res) => {
     let finalTagEmote;
 
     user.tags.forEach((tagInfo) => {
-      if (tagInfo[0].toLowerCase() === req.body.tagName.toLowerCase()) {
+      if (tagInfo.tagName.toLowerCase() === req.body.tagName.toLowerCase()) {
         isValid = true;
-        finalTagName = tagInfo[0].toLowerCase();
-        finalTagEmote = tagInfo[1].toLowerCase();
+        finalTagName = tagInfo.tagName.toLowerCase();
+        finalTagEmote = tagInfo.tagEmote.toLowerCase();
       }
     });
     if (isValid) {
-      await User.updateOne(
+      let tag = {
+        tagName: finalTagName,
+        tagEmote: finalTagEmote,
+      };
+      const updatedUser = await User.updateOne(
         { userId: req.body.userId },
-        {
-          $pull: {
-            tags: [finalTagName, finalTagEmote],
-          },
-        }
+        { $pull: { tags: tag } }
       );
-      res.status(204).json({ message: "success" });
+      res.status(204).json(updatedUser);
     } else {
       res.status(400).json({ message: "error" });
     }
@@ -77,22 +85,26 @@ router.patch("/update", async (req, res) => {
       userId: req.body.userId,
     });
     let isValid = false;
-    let finalTagInfo = user.tags;
-    for (let i = 0; i < user.tags.length; i++) {
-      if (user.tags[i][0].toLowerCase() === req.body.tagName.toLowerCase()) {
+    let finalTagName;
+    let finalTagEmote;
+
+    user.tags.forEach((tagInfo) => {
+      if (tagInfo.tagName.toLowerCase() === req.body.tagName.toLowerCase()) {
         isValid = true;
-        finalTagInfo[i] = [
-          req.body.tagName.toLowerCase(),
-          req.body.tagEmote.toLowerCase(),
-        ];
+        finalTagName = tagInfo.tagName.toLowerCase();
+        finalTagEmote = req.body.tagEmote.toLowerCase();
       }
-    }
+    });
     if (isValid) {
-      await User.updateOne(
-        { userId: req.body.userId },
-        { $set: { tags: finalTagInfo } }
+      const updatedUser = await User.updateOne(
+        { userId: req.body.userId, "tags.tagName": finalTagName },
+        {
+          $set: {
+            "tags.$.tagEmote": finalTagEmote,
+          },
+        }
       );
-      res.status(204).json({ message: "success" });
+      res.status(204).json(updatedUser);
     } else {
       res.status(400).json({ message: "error" });
     }
