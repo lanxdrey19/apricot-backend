287bf512ba14fe5f5c4482f45743739a0fc7ec60
Lance Delos Reyes <lan_drey19@hotmail.com>
2022-02-16 02:40:19 +1300

    :art: restructure tags endpoints

---
+++ daos/tag_dao.js
@@ -0,0 +1,164 @@
+const User = require("../entities/User");
+
+const findTags = async function (userIdentifier) {
+  try {
+    const user = await User.findOne({
+      userId: req.params.userId,
+    });
+
+    return user.tags;
+  } catch (err) {
+    throw new Error("User's tags could not be retrieved");
+  }
+};
+
+const addTagForUser = async function (requestBody) {
+  try {
+    const user = await User.findOne({
+      userId: requestBody.userId,
+    });
+    let isValid = true;
+    user.tags.forEach((tagInfo) => {
+      if (tagInfo.tagName.toLowerCase() === requestBody.tagName.toLowerCase()) {
+        isValid = false;
+      }
+    });
+    if (isValid) {
+      let tag = {
+        tagName: requestBody.tagName.toString().toLowerCase(),
+        tagEmote: requestBody.tagEmote.toString().toLowerCase(),
+      };
+      const updatedUser = await User.updateOne(
+        { userId: requestBody.userId },
+        { $push: { tags: tag } }
+      );
+      return updatedUser;
+    } else {
+      throw new Error("Tag already exists");
+    }
+  } catch (err) {
+    throw new Error("Tag could not be added to user");
+  }
+};
+
+const deleteTagForUser = async function (requestBody) {
+  try {
+    const user = await User.findOne({
+      userId: requestBody.userId,
+    });
+    let isValid = false;
+    let finalTagName;
+    let finalTagEmote;
+
+    user.tags.forEach((tagInfo) => {
+      if (tagInfo.tagName.toLowerCase() === requestBody.tagName.toLowerCase()) {
+        isValid = true;
+        finalTagName = tagInfo.tagName.toLowerCase();
+        finalTagEmote = tagInfo.tagEmote.toLowerCase();
+      }
+    });
+    if (isValid) {
+      let tag = {
+        tagName: finalTagName,
+        tagEmote: finalTagEmote,
+      };
+      const updatedUser = await User.updateOne(
+        { userId: requestBody.userId },
+        { $pull: { tags: tag } }
+      );
+      return updatedUser;
+    } else {
+      throw new Error("Tag could not be found");
+    }
+  } catch (err) {
+    throw new Error("Tag could not be deleted from user");
+  }
+};
+
+const updateTagForUser = async function (requestBody) {
+  try {
+    const user = await User.findOne({
+      userId: requestBody.userId,
+    });
+    let isValid = false;
+    let finalTagName;
+    let finalTagEmote;
+
+    user.tags.forEach((tagInfo) => {
+      if (tagInfo.tagName.toLowerCase() === requestBody.tagName.toLowerCase()) {
+        isValid = true;
+        finalTagName = tagInfo.tagName.toLowerCase();
+        finalTagEmote = requestBody.tagEmote.toLowerCase();
+      }
+    });
+    if (isValid) {
+      const updatedUser = await User.updateOne(
+        { userId: requestBody.userId, "tags.tagName": finalTagName },
+        {
+          $set: {
+            "tags.$.tagEmote": finalTagEmote,
+          },
+        }
+      );
+      return updatedUser;
+    } else {
+      throw new Error("Tag could not be found");
+    }
+  } catch (err) {
+    throw new Error("Tag could not be updated");
+  }
+};
+
+const updateTagUserCard = async function (requestBody) {
+  try {
+    const user = await User.findOne({
+      userId: requestBody.userId,
+    });
+
+    const matchingTag = user.tags.filter(
+      (tag) => tag.tagName === requestBody.newTagName.toLowerCase()
+    );
+    if (matchingTag.length !== 1 && requestBody.newTagName !== "") {
+      res.status(400).json({ message: "error" });
+    }
+
+    user.cards.forEach(async (card) => {
+      if (
+        card.recordedSerial.toLowerCase() ===
+          requestBody.recordedSerial.toLowerCase() &&
+        card.stars === Number(requestBody.stars) &&
+        card.tagName === requestBody.tagName.toLowerCase()
+      ) {
+        const updatedUser = await User.updateOne(
+          {
+            userId: requestBody.userId,
+            cards: {
+              $elemMatch: {
+                templateId: card.templateId,
+                recordedSerial: card.recordedSerial,
+                stars: card.stars,
+                tagName: card.tagName,
+              },
+            },
+          },
+          {
+            $set: {
+              "cards.$.tagName": requestBody.newTagName.toLowerCase(),
+            },
+          }
+        );
+        res.json(updatedUser);
+      }
+    });
+  } catch (err) {
+    throw new Error("Card's tag could not be updated");
+  }
+};
+
+module.exports = {
+  findTags,
+  addTagForUser,
+  deleteTagForUser,
+  updateTagForUser,
+  updateTagUserCard,
+};
--- routes/tags.js
+++ routes/tags.js
@@ -3,43 +3,25 @@ const router = express.Router();
 const Server = require("../entities/Server");
 const User = require("../entities/User");
 const Template = require("../entities/Template");
+const tagInteractor = require("../use_cases/tag_interactor");
+const tagController = require("../controllers/tag_controller");
 
 router.get("/:userId", async (req, res) => {
   try {
-    const user = await User.findOne({
-      userId: req.params.userId,
-    });
-
-    res.status(200).json(user.tags);
+    const tags = await tagInteractor.executeGetUserTags(
+      cardController,
+      req.params.userId
+    );
+    res.status(200).json(tags);
   } catch (err) {
-    res.json({ message: err });
+    res.status(400).json({ message: err });
   }
 });
 
 router.patch("/add", async (req, res) => {
   try {
-    const user = await User.findOne({
-      userId: req.body.userId,
-    });
-    let isValid = true;
-    user.tags.forEach((tagInfo) => {
-      if (tagInfo.tagName.toLowerCase() === req.body.tagName.toLowerCase()) {
-        isValid = false;
-      }
-    });
-    if (isValid) {
-      let tag = {
-        tagName: req.body.tagName.toString().toLowerCase(),
-        tagEmote: req.body.tagEmote.toString().toLowerCase(),
-      };
-      const updatedUser = await User.updateOne(
-        { userId: req.body.userId },
-        { $push: { tags: tag } }
-      );
-      res.json(updatedUser);
-    } else {
-      res.status(400).json({ message: "error" });
-    }
+    const result = await tagInteractor.executeAddTag(cardController, req.body);
+    res.status(200).json(result);
   } catch (err) {
     res.json({ message: err });
   }
@@ -47,115 +29,35 @@ router.patch("/add", async (req, res) => {
 
 router.patch("/delete", async (req, res) => {
   try {
-    const user = await User.findOne({
-      userId: req.body.userId,
-    });
-    let isValid = false;
-    let finalTagName;
-    let finalTagEmote;
-
-    user.tags.forEach((tagInfo) => {
-      if (tagInfo.tagName.toLowerCase() === req.body.tagName.toLowerCase()) {
-        isValid = true;
-        finalTagName = tagInfo.tagName.toLowerCase();
-        finalTagEmote = tagInfo.tagEmote.toLowerCase();
-      }
-    });
-    if (isValid) {
-      let tag = {
-        tagName: finalTagName,
-        tagEmote: finalTagEmote,
-      };
-      const updatedUser = await User.updateOne(
-        { userId: req.body.userId },
-        { $pull: { tags: tag } }
-      );
-      res.status(204).json(updatedUser);
-    } else {
-      res.status(400).json({ message: "error" });
-    }
+    const result = await tagInteractor.executeDeleteTag(
+      cardController,
+      req.body
+    );
+    res.status(200).json(result);
   } catch (err) {
-    res.json({ message: err });
+    res.status(400).json({ message: err });
   }
 });
 
 router.patch("/update", async (req, res) => {
   try {
-    const user = await User.findOne({
-      userId: req.body.userId,
-    });
-    let isValid = false;
-    let finalTagName;
-    let finalTagEmote;
-
-    user.tags.forEach((tagInfo) => {
-      if (tagInfo.tagName.toLowerCase() === req.body.tagName.toLowerCase()) {
-        isValid = true;
-        finalTagName = tagInfo.tagName.toLowerCase();
-        finalTagEmote = req.body.tagEmote.toLowerCase();
-      }
-    });
-    if (isValid) {
-      const updatedUser = await User.updateOne(
-        { userId: req.body.userId, "tags.tagName": finalTagName },
-        {
-          $set: {
-            "tags.$.tagEmote": finalTagEmote,
-          },
-        }
-      );
-      res.status(204).json(updatedUser);
-    } else {
-      res.status(400).json({ message: "error" });
-    }
+    const result = await tagInteractor.executeUpdateUserTag(
+      cardController,
+      req.body
+    );
+    res.status(200).json(result);
   } catch (err) {
-    res.json({ message: err });
+    res.status(400).json({ message: err });
   }
 });
 
 router.patch("/tagcard", async (req, res) => {
   try {
-    const user = await User.findOne({
-      userId: req.body.userId,
-    });
-
-    const matchingTag = user.tags.filter(
-      (tag) => tag.tagName === req.body.newTagName.toLowerCase()
+    const result = await tagInteractor.executeTagUserCard(
+      cardController,
+      req.body
     );
-    if (matchingTag.length !== 1 && req.body.newTagName !== "") {
-      console.log("lmao");
-      res.status(400).json({ message: "error" });
-    }
-
-    user.cards.forEach(async (card) => {
-      if (
-        card.recordedSerial.toLowerCase() ===
-          req.body.recordedSerial.toLowerCase() &&
-        card.stars === Number(req.body.stars) &&
-        card.tagName === req.body.tagName.toLowerCase()
-      ) {
-        const updatedUser = await User.updateOne(
-          {
-            userId: req.body.userId,
-            cards: {
-              $elemMatch: {
-                templateId: card.templateId,
-                recordedSerial: card.recordedSerial,
-                stars: card.stars,
-                tagName: card.tagName,
-              },
-            },
-          },
-          {
-            $set: {
-              "cards.$.tagName": req.body.newTagName.toLowerCase(),
-            },
-          }
-        );
-        console.log(updatedUser);
-        res.json(updatedUser);
-      }
-    });
+    res.status(200).json(result);
   } catch (err) {
     res.json({ message: err });
   }
