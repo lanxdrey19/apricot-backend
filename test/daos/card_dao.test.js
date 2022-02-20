e12e98642add2f5e726d220abf240dd517367376
Lance Delos Reyes <lan_drey19@hotmail.com>
2022-02-20 23:36:00 +1300

    :white_check_mark: add tests for invalid inputs for card dao

--- test/daos/card_dao.test.js
+++ test/daos/card_dao.test.js
@@ -14,6 +14,16 @@ const cardObject = {
   recordedSerial: "50",
   stars: 3,
 };
+const invalidCardObject = {
+  name: "name",
+  group: "group",
+  era: "era",
+  photo: "photo",
+  logo: "logo",
+  tagName: "tag",
+  recordedSerial: "50",
+  stars: 5,
+};
 const newCardObject = {
   name: "newName",
   group: "newGroup",
@@ -33,7 +43,7 @@ const templateObject = {
   serial: "52",
 };
 
-describe("card_dao", function () {
+describe("card_dao valid tests", function () {
   beforeEach(function () {
     sandbox.stub(User, "findOne").resolves({
       userId: "validServerIdentifier",
@@ -138,5 +148,34 @@ describe("card_dao", function () {
   it("update card information", async function () {
     const result = await cardDAO.updateCardStars(cardObject);
     expect(result).to.have.property("updated");
+  });
+});
+
+describe("card_dao invalid tests", function () {
+  beforeEach(function () {
+    sandbox.stub(User, "findOne").resolves({
+      userId: "validServerIdentifier",
+      cards: [cardObject],
+      tokens: 50,
+    });
+
+    sandbox.stub(User, "updateOne").resolves({
+      updated: "updated",
+    });
+
+    sandbox.stub(Template, "findById").resolves(templateObject);
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  it("update card information", async function () {
+    try {
+      await cardDAO.updateCardStars(invalidCardObject);
+    } catch (error) {
+      expect(error).to.be.instanceOf(Error);
+      expect(error.message).to.eql("Card's stars could not be incremented");
+    }
   });
 });
